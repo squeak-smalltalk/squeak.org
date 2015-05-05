3d12cb97b14e2a91c8f7ab1549ef906bc820b6c5
Fabio Niephaus <code@fniephaus.com>
2015-05-05 18:50:09 +0200

    Add simple screenshot slideshow

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,48 @@
 var ignore_scrolling = false;
 
 
+function enable_screenshot_buttons() {
+    $('.screenshot-button').click(function(e) {
+        e.preventDefault();
+        $(this).parents('.slideshow-item').hide();
+
+        var is_right = $(this).data('target') == 'right';
+        var element = $(this).parents('.slideshow-item');
+        if (is_right) {
+            element = element.next();
+        } else {
+            element = element.prev();
+        }
+        if (element.length > 0) {
+            element.show();
+        } else {
+            if (is_right) {
+                $('#screenshots div:first-child').show();
+            } else {
+                $('#screenshots div:last-child').show();
+            }
+        }
+    })
+}
+
+
 function load_content(){
+    var slideshow_item = $('#slideshow-item').html();
+    Mustache.parse(slideshow_item);   // optional, speeds up future uses
+
     var simple_box_template = $('#simple-box').html();
     Mustache.parse(simple_box_template);   // optional, speeds up future uses
 
     $.getJSON('content.json', function(content) {
         $('#welcome-content').html(content['welcome']);
+
+        $.each(content['screenshots'], function(idx, screenshot) {
+            $('#screenshots').append(Mustache.render(slideshow_item, screenshot));
+        });
+        debugger
+        $('#screenshots div:first-child').show();
+
+        enable_screenshot_buttons();
 
         $.each(content['sections'], function(section, section_data) {
             $.each(section_data, function(idx, data) {
