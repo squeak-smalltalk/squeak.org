316fcd9221143f93f07d5395ae27585f8698a90e
Fabio Niephaus <code@fniephaus.com>
2015-05-05 16:17:19 +0200

    Enable scrolling and improve responsiveness

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,13 @@
+var ignore_scrolling = false;
+
 $(function() {
     $('ul.nav li a, a.navbar-brand').click(function(e) {
         e.preventDefault();
         var target = $(this).attr('href');
-        $('body').animate({ scrollTop: $(target).offset().top - 70 }, 'fast');
+        ignore_scrolling = true;
+        $('body').animate({
+            scrollTop: $(target).offset().top - 70
+        }, 'fast', function() {ignore_scrolling = false;});
 
         $('ul.nav li').each(function() {
             $(this).removeClass('active');
@@ -13,6 +18,26 @@ $(function() {
             $(this).parent().addClass('active');
         }
     });
+
+    $(window).scroll(function() {
+        if (ignore_scrolling)
+            return;
+        var scroll_position = $(document).scrollTop() + 80;
+        $('ul.nav li a').each(function () {
+            var selected = false;
+            $($(this).attr("href")).each(function () {
+                if ($(this).position().top <= scroll_position && $(this).position().top + $(this).height() + 40 > scroll_position) {
+                    selected = true;
+                }
+            });
+            if (selected) {
+                $(this).parent().addClass("active");
+            } else {
+                $(this).parent().removeClass("active");
+            }
+        });
+    });
+
     hljs.configure({languages: ['smalltalk']});
     hljs.initHighlighting();
 });
