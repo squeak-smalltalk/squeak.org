dfeac598dc7edcd83473d7ff835c41dbee251ad6
Fabio Niephaus <code@fniephaus.com>
2015-06-04 12:19:00 -0700

    Improve screenshot slideshow

--- static/js/ready.js
+++ static/js/ready.js
@@ -1,41 +1,40 @@
+function rotateScreenshot(moveRight) {
+    var current = $('.slideshow-item:visible');
+    var next = (moveRight ? current.next() : current.prev());
+    $('.slideshow-item').hide();
+    if (next.length > 0) {
+        next.show();
+    } else {
+        if (moveRight) {
+            $('#screenshots div:first-child').show();
+        } else {
+            $('#screenshots div:last-child').show();
+        }
+    }
+}
+
 function enableScreenshotButtons() {
+    $('.screenshot-img').click(function(e) {
+        e.preventDefault();
+        var moveRight = event.offsetX > $('img', this).width() / 2;
+        rotateScreenshot(moveRight);
+    });
     $('.screenshot-button').click(function(e) {
         e.preventDefault();
-        $(this).parents('.slideshow-item').hide();
-
-        var isRight = $(this).data('target') === 'right';
-        var element = $(this).parents('.slideshow-item');
-        if (isRight) {
-            element = element.next();
-        } else {
-            element = element.prev();
-        }
-        if (element.length > 0) {
-            element.show();
-        } else {
-            if (isRight) {
-                $('#screenshots div:first-child').show();
-            } else {
-                $('#screenshots div:last-child').show();
-            }
-        }
+        var moveRight = $(this).data('target') === 'right';
+        rotateScreenshot(moveRight);
     });
 }
 
 function enableKeyboardNavigation() {
     $(window).keyup(function(e) {
         var key = e.which | e.keyCode;
-        if(key === 37) {
-            clickScreenshotButton('left');
-        }
-        else if(key === 39) {
-            clickScreenshotButton('right');
+        if (key === 37) {
+            rotateScreenshot(false);
+        } else if (key === 39) {
+            rotateScreenshot(true);
         }
     });
-}
-
-function clickScreenshotButton(direction) {
-    $('.slideshow-item:visible .screenshot-button[data-target="' + direction + '"]').click();
 }
 
 $(function() {
@@ -44,13 +43,11 @@ $(function() {
         e.preventDefault();
         $('#paypal-donations').submit();
     });
-
     // Enable screenshots
     if ($('#screenshots').length > 0) {
         enableScreenshotButtons();
         enableKeyboardNavigation();
     }
-
     // Enable SVG fallback to PNG
     svgeezy.init(false, 'png');
 });
